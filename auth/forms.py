5ec30437c1b988ad156b9c8ff7883321590436ed
Robert Myers <robert@keyingredient.com>
2012-03-22 16:31:14 -0500

    Fixing the authentication form

--- auth/forms.py
+++ auth/forms.py
@@ -1,14 +1,55 @@
 
 from google.appengine.ext.db import djangoforms
 from django import forms
+from django.contrib.auth import authenticate
 
 from models import User
 
 class AuthenticationForm(forms.Form):
-    """Simple authentication form for email/password."""
-    
+    """
+    Base class for authenticating users. Extend this to get a form that accepts
+    username/password logins.
+    """
     email = forms.EmailField(required=True)
-    password = forms.CharField(required=True, widget=forms.PasswordInput)
+    password = forms.CharField(label=_("Password"), widget=forms.PasswordInput)
+
+    def __init__(self, request=None, *args, **kwargs):
+        """
+        If request is passed in, the form will validate that cookies are
+        enabled. Note that the request (a HttpRequest object) must have set a
+        cookie with the key TEST_COOKIE_NAME and value TEST_COOKIE_VALUE before
+        running this validation.
+        """
+        self.request = request
+        self.user_cache = None
+        super(AuthenticationForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+
+        if email and password:
+            self.user_cache = authenticate(email=email, password=password)
+            if self.user_cache is None:
+                raise forms.ValidationError(_("Please enter a correct username and password. Note that both fields are case-sensitive."))
+            elif not self.user_cache.is_active:
+                raise forms.ValidationError(_("This account is inactive."))
+        self.check_for_test_cookie()
+        return self.cleaned_data
+
+    def check_for_test_cookie(self):
+        if self.request and not self.request.session.test_cookie_worked():
+            raise forms.ValidationError(
+                _("Your Web browser doesn't appear to have cookies enabled. "
+                  "Cookies are required for logging in."))
+
+    def get_user_id(self):
+        if self.user_cache:
+            return self.user_cache.id
+        return None
+
+    def get_user(self):
+        return self.user_cache
 
     
 class SignupForm(djangoforms.ModelForm): 
