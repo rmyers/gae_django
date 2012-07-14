9802e39940ac1ab7dfd024a0bf557da8ebced28d
Robert Myers <robert@keyingredient.com>
2012-07-13 23:27:07 -0500

    Changing admin for users to just display the safe username property

--- auth/admin.py
+++ auth/admin.py
@@ -3,5 +3,5 @@ from gae_django import admin
 
 from models import User
 
-admin.site.register(User, list_display=['first_name', 'last_name'], 
+admin.site.register(User, list_display=['username'], 
     list_filter=['is_superuser'], readonly_fields=['password'])
